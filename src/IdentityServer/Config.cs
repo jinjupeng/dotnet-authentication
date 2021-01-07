cb91ccc451b7a83427e5d0089d91125f65b5e97e
jinjupeng <im.jp@outlook.com>
2021-01-07 21:18:49 +0800

    新增IdentityServer服务

---
+++ src/IdentityServer/Config.cs
@@ -0,0 +1,69 @@
+﻿using IdentityServer4.Models;
+using IdentityServer4.Test;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityServer
+{
+    public class Config
+    {
+        /// <summary>
+        /// 这个 Authorization Server 保护了哪些 API （资源）
+        /// </summary>
+        /// <returns></returns>
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>() {
+                new ApiResource("api1", "My API")
+            };
+        }
+
+        public static IEnumerable<ApiScope> ApiScopes =>
+        new List<ApiScope>
+        {
+            new ApiScope("api1", "My API")
+        };
+
+        /// <summary>
+        /// 哪些客户端 Client（应用） 可以使用这个 Authorization Server
+        /// </summary>
+        /// <returns></returns>
+        public static IEnumerable<Client> GetClients()
+        {
+            /*
+             * 客户端代码中的ClientId和ClientSecret可以视为应用程序本身的登录名和密码，它将你的应用程序
+             * 标识到IdentityServer服务器中，以便它知道哪个应用程序正在尝试与其连接。
+             */
+            return new[]
+            {
+                    new Client
+                    {
+                        ClientId = "client",//定义客户端 Id
+                        ClientSecrets = new [] { new Secret("secret".Sha256()) },//Client用来获取token
+                        AllowedGrantTypes = GrantTypes.ResourceOwnerPasswordAndClientCredentials,//这里使用的是通过用户名密码和ClientCredentials来换取token的方式. ClientCredentials允许Client只使用ClientSecrets来获取token. 这比较适合那种没有用户参与的api动作
+                        //AllowedGrantTypes = GrantTypes.ClientCredentials,
+                        AllowedScopes = new [] { "api1" }// 允许访问的 API 资源
+                    }
+            };
+        }
+
+        /// <summary>
+        /// 指定可以使用 Authorization Server 授权的 Users（用户）
+        /// </summary>
+        /// <returns></returns>
+        public static IEnumerable<TestUser> Users()
+        {
+            return new[]
+            {
+                    new TestUser
+                    {
+                        SubjectId = "1",
+                        Username = "admin",
+                        Password = "admin"
+                    }
+            };
+        }
+    }
+}
---
+++ src/IdentityServer/Startup.cs
@@ -0,0 +1,70 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityServer
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+
+            /*
+             * IdentityServer是作为授权服务器，比如OAuth2等等
+             */
+            var builder = services.AddIdentityServer(options =>
+            {
+                options.Events.RaiseErrorEvents = true;
+                options.Events.RaiseInformationEvents = true;
+                options.Events.RaiseFailureEvents = true;
+                options.Events.RaiseSuccessEvents = true;
+            })
+                .AddTestUsers(Config.Users().ToList())
+                .AddInMemoryApiResources(Config.GetApiResources())
+                .AddInMemoryClients(Config.GetClients())
+                .AddInMemoryApiScopes(Config.ApiScopes);
+            builder.AddDeveloperSigningCredential();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
