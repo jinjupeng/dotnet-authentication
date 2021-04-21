6f7f00f55fb0bee9f417062c7a5e4b72168be7dd
jinjupeng <im.jp@outlook.com>
2021-04-21 09:08:05 +0800

    集成GitHub第三方登录

---
+++ src/GitHub/Controllers/AuthController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GitHub.Controllers
+{
+    /// <summary>
+    /// GitHub第三方登录
+    /// </summary>
+    [Route("[controller]/[action]")]
+    public class AuthController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login(string returnUrl = "/")
+        {
+            return Challenge(new AuthenticationProperties() { RedirectUri = returnUrl });
+        }
+    }
+}
---
+++ src/GitHub/Pages/Index.cshtml.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Octokit;
+using Octokit.Internal;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Client.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public IReadOnlyList<Repository> Repositories { get; set; }
+
+        public IReadOnlyList<Repository> StarredRepos { get; set; }
+
+        public IReadOnlyList<User> Followers { get; set; }
+
+        public IReadOnlyList<User> Following { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                string accessToken = await HttpContext.GetTokenAsync("access_token");
+
+                var github = new GitHubClient(new ProductHeaderValue("AspNetCoreGitHubAuth"), new InMemoryCredentialStore(new Credentials(accessToken)));
+                Repositories = await github.Repository.GetAllForCurrent();
+
+                StarredRepos = await github.Activity.Starring.GetAllForCurrent();
+
+                Followers = await github.User.Followers.GetAllForCurrent();
+                Following = await github.User.Followers.GetAllFollowingForCurrent();
+            }
+        }
+    }
+}
